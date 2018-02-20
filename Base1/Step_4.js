7ee3765272b52ef66e3cc90be405aac7040ca831
amrgharz <ammor.ghr@gmail.com>
2018-02-20 15:33:32 +0200

    fourth commit

---
+++ Base1/Step_4.js
@@ -0,0 +1,10 @@
+console.log("")
+var username = document.getElementById("name")
+var surname = document.getElementById("surname")
+var city = document.getElementById("city")
+var validate = document.getElementById("validate")
+
+function add(){
+    alert("name :" + username.value + "\nsurename :" + surname.value + "\ncity :"+ city.value)
+}
+validate.addEventListener('click' ,add)
