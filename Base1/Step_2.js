74713de8a33192a3e8380f315cf63bb7af8cfaa0
amrgharz <ammor.ghr@gmail.com>
2018-02-20 11:01:53 +0200

    second commit

---
+++ Base1/Step_2.js
@@ -0,0 +1,10 @@
+console.log(" ")
+var name = "amr"
+var surename = "gharz"
+var city ="bierut"
+ function box (){
+     alert (
+         "name: "+ name + "\nsurname: "+ surename +"\ncity: " + city
+     )
+ }
+ box();
