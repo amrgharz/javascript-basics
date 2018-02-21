2bb25f3875919fbf0d13340bc7b6e72b18c8dca2
amrgharz <ammor.ghr@gmail.com>
2018-02-21 12:32:38 +0200

    1base2

---
+++ Base2/step 1/index.js
@@ -0,0 +1,12 @@
+console.log(" ")
+var img =document.getElementById("image1")
+img.addEventListener('onmouseover' , function (){
+    
+})
+
+
+
+
+
+// I applied this on the html and it is working 
+//onmouseover="src='images/image1_2.jpg'" onmouseout="src='images/image1.jpg'"
