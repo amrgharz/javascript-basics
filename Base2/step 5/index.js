bd893160cf675bc9dca5f754056b32f17aabd39f
amrgharz <ammor.ghr@gmail.com>
2018-02-26 10:36:15 +0200

    all good

--- Base2/step 5/index.js
+++ Base2/step 5/index.js
@@ -1,7 +1,10 @@
-console.log("j")
 const images = document.querySelectorAll (".container img")
-images.forEach ((img , i )=>{
+    images.forEach((img , i)=>{
+
     img.addEventListener('mouseover' , function (){
-        this.src= "images/image" + ++i +"_2.jpg";
+        this.src= "images/image" + (i+1) +"_2.jpg";
     })
 })
+
+
+
